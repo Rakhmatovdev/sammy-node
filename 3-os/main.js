b6546eb021e31db0c53d97da96ae2db5d5ee0fc8
Jasur <rakhmatovjasur3@gmail.com>
2024-05-04 22:25:18 +0500

    os module created

---
+++ 3-os/main.js
@@ -0,0 +1,19 @@
+const os = require('node:os');
+
+//Platform
+// console.log(os.platform()); //darwin
+
+// indformation
+// console.log(os.cpus());
+
+//arxitecture
+// console.log(os.arch());
+
+//free memory
+// console.log(os.freemem());
+
+//total memory
+// console.log(os.totalmem());
+
+//homedir
+console.log(os.homedir());
