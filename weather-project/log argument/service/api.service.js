8fe81def1a9c98551adba82c861a74fdbb23ef5d
Jasur <rakhmatovjasur3@gmail.com>
2024-05-05 15:08:26 +0500

    Axios version

--- weather-project/log argument/service/api.service.js
+++ weather-project/log argument/service/api.service.js
@@ -7,34 +7,56 @@ const getWeather = async (city) => {
     throw new Error("Api doen't not exist -t [API_KEY] for saving token");
   }
 //axios version
-// const response=await axios.get("https://api.openweathermap.org/data/2.5/weather",{
-//     params:{
-//         q:city,
-//         appid:token,
-//         lang:"en",
-//         units:"metric"
-//     }
-// })
+const {data}=await axios.get("https://api.openweathermap.org/data/2.5/weather",{
+    params:{
+        q:city,
+        appid:token,
+        lang:"en",
+        units:"metric"
+    }
+})
 
-// console.log(response.data);
+return data
 
-// node version 
-  const url=new URL("https://api.openweathermap.org/data/2.5/weather")
-  url.searchParams.append("q", city);
-  url.searchParams.append("appid", token);
-  url.searchParams.append("lang", "en");
-  url.searchParams.append("units", "metric");
-
-  https.get(url, (response) => {
-    let res = "";
-    response.on("data", (chunk) => {
-      res += chunk;
-    });
-    response.on("end", () => {
-      console.log(res);
-    });
-  });
 };
 
 
 export { getWeather };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// node version 
+//   const url=new URL("https://api.openweathermap.org/data/2.5/weather")
+//   url.searchParams.append("q", city);
+//   url.searchParams.append("appid", token);
+//   url.searchParams.append("lang", "en");
+//   url.searchParams.append("units", "metric");
+
+//   https.get(url, (response) => {
+//     let res = "";
+//     response.on("data", (chunk) => {
+//       res += chunk;
+//     });
+//     response.on("end", () => {
+//       console.log(res);
+//     });
+//   });
